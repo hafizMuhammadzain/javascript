ecdcad41302c10091f00302e51f979a39ca1e9a8
Muhammad Zain <zainzahid42401@gmail.com>
2024-03-15 02:23:32 +0500

    add chap 01-16 assignment

---
+++ chap01/app.js
@@ -0,0 +1,4 @@
+var fault ="Error! please enter a valid password"
+var happycoder ="welcom to js land happy coding"   
+alert(fault)
+alert(happycoder)
---
+++ chap03/app.js
@@ -0,0 +1,8 @@
+var age = "Hi I'm 20 Year Old"
+alert(age);
+var forExample = "You Have Visited This Site 14 Time"
+alert(forExample);
+
+var birthYear = 2004;
+alert("Your birth year is: " + birthYear);
+document.getElementById("birthYearMessage").innerHTML = "My birth year is: " + birthYear;
